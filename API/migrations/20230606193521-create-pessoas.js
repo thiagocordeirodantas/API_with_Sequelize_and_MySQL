faf3b018ca64afd607e181f4ca68a62fcd89f908
Thiago <thiiagoccordeirodantas@gmail.com>
2023-06-06 21:37:49 -0300

    rodando as migracoes e inserindo um usuario na tabela

--- API/migrations/20230606193521-create-pessoas.js
+++ API/migrations/20230606193521-create-pessoas.js
@@ -24,7 +24,7 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
-      },
+      }, 
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
